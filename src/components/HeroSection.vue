f2970f806ae07cdd677a48f9d9199af5797fe9f8
MAGICX <majikx.prod0@gmail.com>
2024-12-13 01:13:26 +0500

    feat: add hacker mode

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,12 +3,14 @@ import TelegramIcon from '@/components/icons/TelegramIcon.vue'
 import GithubIcon from '@/components/icons/GithubIcon.vue'
 import TwitchIcon from '@/components/icons/TwitchIcon.vue'
 
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, onMounted, onUnmounted } from "vue";
 import { cn } from "@/lib/utils";
+
+const isDeveloper = ref(false);
 
 const props = defineProps({
   gradientSize: { type: Number, default: 200 },
-  gradientColor: { type: String, default: "#262626" },
+  gradientColor: { type: String, default: "#111111" },
   gradientOpacity: { type: Number, default: 0.8 },
   class: { type: String, default: "" },
   slotClass: String,
@@ -16,6 +18,12 @@ const props = defineProps({
 
 const mouseX = ref(-props.gradientSize * 10);
 const mouseY = ref(-props.gradientSize * 10);
+
+const handleKeyDown = (event) => {
+  if (event.ctrlKey && event.altKey && (event.key === 'h' || event.key === 'р')) {
+    isDeveloper.value = !isDeveloper.value; // Переключаем значение
+  }
+};
 
 function handleMouseMove(e: MouseEvent) {
   const target = e.currentTarget as HTMLElement;
@@ -30,8 +38,13 @@ function handleMouseLeave() {
 }
 
 onMounted(() => {
+  window.addEventListener('keydown', handleKeyDown);
   mouseX.value = -props.gradientSize * 10;
   mouseY.value = -props.gradientSize * 10;
+});
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeyDown);
 });
 
 const backgroundStyle = computed(() => {
@@ -54,28 +67,72 @@ const backgroundStyle = computed(() => {
       @mouseleave="handleMouseLeave"
     >
       <div :class="cn('content-center w-full z-10', props.slotClass)">
-        <div class="cursor-pointer flex-col items-center justify-center whitespace-nowrap">
-          <div class="title">HAXGUN</div>
-          <div class="subtitle">Little bit of a fullstack developer</div>
-          <a href="mailto:haxgun@vk.com" class="email">haxgun@vk.com</a>
-          <div class="flex-col items-center justify-center whitespace-nowrap content-center social">
-            <li class="social-icon content-center">
-              <a href="https://www.twitch.tv/magicxcmd" target="__blank">
-                <TwitchIcon />
-              </a>
-            </li>
-            <li class="social-icon">
-              <a href="https://t.me/haxgun" target="__blank">
-                <TelegramIcon />
-              </a>
-            </li>
-            <li class="social-icon">
-              <a href="https://github.com/haxgun" target="__blank">
-                <GithubIcon />
-              </a>
-            </li>
+        <Transition mode="out-in">
+          <div v-if="!isDeveloper" class="cursor-pointer flex-col items-center justify-center whitespace-nowrap">
+            <div class="title">HAXGUN</div>
+            <div class="subtitle">Little bit of a fullstack developer</div>
+            <a href="mailto:haxgun@vk.com" class="email">haxgun@vk.com</a>
+            <div class="flex-col items-center justify-center whitespace-nowrap content-center social">
+              <li class="social-icon content-center">
+                <a href="https://www.twitch.tv/magicxcmd" target="__blank">
+                  <TwitchIcon />
+                </a>
+              </li>
+              <li class="social-icon">
+                <a href="https://t.me/haxgun" target="__blank">
+                  <TelegramIcon />
+                </a>
+              </li>
+              <li class="social-icon">
+                <a href="https://github.com/haxgun" target="__blank">
+                  <GithubIcon />
+                </a>
+              </li>
+            </div>
           </div>
-        </div>
+          <div v-else class="cursor-pointer flex-col items-center justify-center whitespace-nowrap">
+            <div class="title develop">
+              <img class="logo" src="/logo.png" alt="">
+            </div>
+            <div class="flex-col items-center justify-center whitespace-nowrap content-center tools">
+              <li class="social-icon">
+                <a href="/outline" target="__blank">
+                  Outline
+                </a>
+              </li>
+              <li class="social-icon">
+                <a href="/nekoray" target="__blank">
+                  Nekoray
+                </a>
+              </li>
+              <li class="social-icon">
+                <a href="/aio" target="__blank">
+                  Windows AIO
+                </a>
+              </li>
+              <li class="social-icon">
+                <a href="/spotify" target="__blank">
+                  Spotify
+                </a>
+              </li>
+              <li class="social-icon">
+                <a href="/discord" target="__blank">
+                  Discord
+                </a>
+              </li>
+              <li class="social-icon">
+                <a href="/notepad" target="__blank">
+                  Notepad
+                </a>
+              </li>
+              <li class="social-icon">
+                <a href="/tools" target="__blank">
+                  Tools
+                </a>
+              </li>
+            </div>
+          </div>
+        </Transition>
       </div>
       <div
         class="pointer-events-none absolute inset-0 rounded-xl opacity-0 transition-opacity duration-300 group-hover:opacity-100"
@@ -90,6 +147,16 @@ const backgroundStyle = computed(() => {
 </template>
 
 <style lang="scss" scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s linear;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
 .hero {
   height: 100vh;
   display: flex;
@@ -104,6 +171,15 @@ const backgroundStyle = computed(() => {
   font-weight: 600;
   margin-bottom: 5px;
   color: hsl(0, 0%, 100%);
+
+  &.develop {
+    display: flex;
+    justify-content: center;
+
+    .logo {
+      width: 500px;
+    }
+  }
 }
 
 .subtitle {
@@ -140,5 +216,35 @@ const backgroundStyle = computed(() => {
   align-items: center;
   flex-direction: row;
   gap: 14px;
+}
+
+.tools {
+  margin-top: 36px;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 14px;
+
+  a {
+    color: #A00411;
+    transition: color .2s;
+
+    &:hover {
+      color: #6b000a;
+    }
+  }
+
+
+  li:not(:last-child)::after {
+    content: " |";
+    margin-left: 5px;
+    color: #A00411;
+  }
+
+  .social-icon {
+    color: #fff;
+  }
 }
 </style>
