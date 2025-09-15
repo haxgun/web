2ed4e4a54694073a44730ef9b8b511dce2589c3d
Misha Gusev <majikx.prod0@gmail.com>
2025-09-16 01:20:52 +0500

    chore: rm useless comment lol

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,6 @@
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
-// import { useHead, useSeoMeta } from '@unhead/vue'
-
-// useHead({
-//   title: metaTitle,
-//   titleTemplate: (title) => (title === metaTitle ? title : `${title} - ${titleMain}`),
-//   meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-// })
-
-// useSeoMeta({
-//   title: metaTitle,
-//   description: metaDescription,
-//   viewport: 'width=device-width, initial-scale=1',
-//   ogTitle: metaTitle,
-//   ogDescription: metaDescription,
-//   ogImage: metaImg,
-//   twitterCard: 'summary_large_image',
-//   twitterTitle: metaTitle,
-//   twitterDescription: metaDescription,
-//   twitterImage: metaImg,
-//   author: 'MAGICX, misha@valory.su',
-//   keywords: metaKeywords,
-// })
-//
 </script>
 
 <template>
