271a2f36ebce8b77d08424a94b54621f8b6ae485
Misha Gusev <majikx.prod0@gmail.com>
2025-09-16 00:55:39 +0500

    chore: rm scale on hover

--- src/components/ui/Photo.vue
+++ src/components/ui/Photo.vue
@@ -7,6 +7,6 @@ const props = defineProps({
 <template>
   <img
     :src="props.src"
-    class="inline-block size-24 mb-4 rounded-xl object-cover saturate-0 contrast-110 border border-white/5 hover:size-48 hover:saturate-100 hover:contrast-100 hover:rounded-2xl duration-500 ease-[cubic-bezier(0.77,0,0.175,1)]"
+    class="inline-block size-24 rounded-xl object-cover saturate-0 contrast-110 border border-white/5 hover:saturate-100 hover:contrast-100 duration-500 ease-[cubic-bezier(0.77,0,0.175, 1)]"
   />
 </template>
