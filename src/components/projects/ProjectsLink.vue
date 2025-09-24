5b85b1f07203950cd1968c4b309dc163f6d17016
Misha Gusev <majikx.prod0@gmail.com>
2025-09-25 01:04:43 +0500

    fix: display for phone

--- src/components/projects/ProjectsLink.vue
+++ src/components/projects/ProjectsLink.vue
@@ -6,20 +6,22 @@ const props = defineProps<IProject>()
 
 <template>
   <a
-    class="group contact-url w-fit cursor-pointer gap-2.5 py-px px-2 hover:bg-white/10 -ml-2 hover:rounded-sm transition-all inline-flex items-center"
+    class="group contact-url w-fit cursor-pointer gap-2.5 py-px px-2 hover:bg-white/10 -ml-2 hover:rounded-sm transition-all flex"
     :href="props.link"
     target="_blank"
   >
-    <span class="inline-flex items-center gap-2">
+    <span class="inline-flex h-fit items-center gap-2">
       <ProjectStatus v-if="props.stage" :stage="props.stage" />
       <span class="font-base group-hover:text-neutral-200 text-neutral-400 transition-colors">
         {{ props.year }}
       </span>
     </span>
 
-    <span class="font-semibold text-neutral-600 dark:text-white">{{ props.title }}</span>
-    <span class="group-hover:text-neutral-200 text-neutral-400 text-sm transition-colors">
-      {{ props.description }}
-    </span>
+    <div class="flex flex-col md:flex-row md:items-center md:gap-2.5">
+      <span class="font-semibold text-neutral-600 dark:text-white">{{ props.title }}</span>
+      <span class="group-hover:text-neutral-200 text-neutral-400 text-sm transition-colors">
+        {{ props.description }}
+      </span>
+    </div>
   </a>
 </template>
