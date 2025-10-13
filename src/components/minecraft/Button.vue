3bf652fa62f2009cfc259e8263760078673c09e8
Misha Gusev @haxgun <misha@valory.su>
2025-10-14 02:44:56 +0500

    feat(minecraft): add button component

---
+++ src/components/minecraft/Button.vue
@@ -0,0 +1,78 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  as?: 'a' | 'button'
+  href?: string
+}>(), {
+  as: 'a'
+})
+</script>
+
+<template>
+  <component :is="props.as" class="MCButton" :href="props.as === 'a' || props.href ? props.href : undefined"
+    :target="props.as === 'a' ? '_blank' : undefined" :rel="props.as === 'a' ? 'noopener' : undefined">
+    <span>
+      <slot />
+    </span>
+  </component>
+</template>
+
+
+
+<style scoped>
+.MCButton {
+  background: #3c8527;
+  box-shadow: inset 0 -6px 0 #2a641c, inset 0 6px 0 #52a535;
+  color: #ffffff;
+  font-weight: 700;
+  letter-spacing: .03em;
+  text-transform: uppercase;
+
+  cursor: pointer;
+
+  border: 2px solid #262423;
+  display: inline-flex;
+  align-items: center;
+  font-size: 1.125rem;
+  line-height: 1.25rem;
+  min-width: 4.75rem;
+  padding: .9375rem 1.75rem;
+  padding-right: 1rem;
+  text-decoration: none;
+  text-align: center;
+
+  -webkit-user-select: none;
+  user-select: none;
+  text-shadow: 0 2px 0 #505050;
+}
+
+.MCButton:hover {
+  background: linear-gradient(0deg, rgba(0, 0, 0, .1), rgba(0, 0, 0, .1)), #3c8527;
+}
+
+.MCButton:focus:active {
+  box-shadow: 0 0 0 1px #ffffff;
+}
+
+.MCButton:active {
+  background-color: #2a641c;
+}
+
+.MCButton:focus {
+  outline: 2px solid #ffffff;
+  outline-offset: -2px;
+}
+
+.MCButton::after {
+  background-color: #ffffff;
+  clip-path: polygon(0% 0%, 0% 25%, 14.286% 25%, 14.286% 50%, 28.572% 50%, 28.572% 75%, 42.858% 75%, 42.858% 100%, 57.142% 100%, 57.142% 75%, 71.428% 75%, 71.428% 50%, 85.714% 50%, 85.714% 25%, 100% 25%, 100% 0%, 85.714% 0%, 85.714% 25%, 71.428% 25%, 71.428% 50%, 57.142% 50%, 57.142% 75%, 42.858% 75%, 42.858% 50%, 28.572% 50%, 28.572% 25%, 14.286% 25%, 14.286% 0%);
+  content: "";
+  display: inline-block;
+  flex-basis: 14px;
+  flex-grow: 0;
+  flex-shrink: 0;
+  height: 8px;
+  transform: rotate(-90deg);
+  width: 14px;
+  margin-left: 10px;
+}
+</style>
--- src/pages/MinecraftPage.vue
+++ src/pages/MinecraftPage.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { onMounted, type Ref, ref } from 'vue'
 import { useTitle, useFavicon } from '@vueuse/core'
-import { Dialog, VideoBackground } from '@/components/minecraft'
+import { Dialog, VideoBackground, Button } from '@/components/minecraft'
 
 
 interface Link {
@@ -41,35 +41,31 @@ onMounted(() => {
         class="flex items-center justify-center flex-wrap gap-5 w-fit bg-[#171615] border-0 border-t-5 border-[#3D3938] p-5">
         <Dialog v-model="serverModal" title="Сервера"
           description="Выберите версию сервера, на котором хотите практиковаться">
-          <a class="MCButton" href="https://drive.google.com/file/d/1X0JG-sNaL-3P9TwY63I9EBaHMJB1MJFM/view?usp=sharing"
-            target="_blank" rel="noopener">
+          <Button href="https://drive.google.com/file/d/1X0JG-sNaL-3P9TwY63I9EBaHMJB1MJFM/view?usp=sharing">
             <span>Версия 1.11.2</span>
-          </a>
-          <a class="MCButton" href="https://drive.google.com/file/d/1yeGIIKUzATAwhrR1TGHuvsQSSd8tEoO-/view?usp=sharing"
-            target="_blank" rel="noopener">
+          </Button>
+          <Button href="https://drive.google.com/file/d/1yeGIIKUzATAwhrR1TGHuvsQSSd8tEoO-/view?usp=sharing">
             <span>Версия 1.16.5</span>
-          </a>
+          </Button>
         </Dialog>
-        <button @click="() => { serverModal = true }" class="MCButton">
+        <Button as="button" @click="() => { serverModal = true }">
           <span>Сервера</span>
-        </button>
+        </Button>
         <Dialog v-model="minePiModal" title="MinePi"
           description="Выберите версию библиотеки исходя из версии Python. Проверить версию Python можно в терминале командой &quot;python --version&quot;">
-          <a class="MCButton" href="https://github.com/py3minepi/py3minepi/archive/refs/heads/master.zip"
-            target="_blank" rel="noopener">
+          <Button as="a" href="https://github.com/py3minepi/py3minepi/archive/refs/heads/master.zip">
             <span>Меньше 3.9 </span>
-          </a>
-          <a class="MCButton" href="https://github.com/haxgun/py3minepi-reload/archive/refs/heads/master.zip"
-            target="_blank" rel="noopener">
+          </Button>
+          <Button as="a" href="https://github.com/haxgun/py3minepi-reload/archive/refs/heads/master.zip">
             <span>Больше или равно 3.9</span>
-          </a>
+          </Button>
         </Dialog>
-        <button @click="() => { minePiModal = true }" class="MCButton">
+        <Button as="button" @click="() => { minePiModal = true }" class="MCButton">
           <span>MinePi</span>
-        </button>
-        <a v-for="link in links" :key="link.url" class="MCButton" :href="link.url" target="_blank" rel="noopener">
+        </Button>
+        <Button as="a" v-for="link in links" :key="link.url" :href="link.url">
           <span>{{ link.name }}</span>
-        </a>
+        </Button>
       </div>
     </div>
 
@@ -85,63 +81,3 @@ onMounted(() => {
     </footer>
   </VideoBackground>
 </template>
-
-
-<style scoped>
-.MCButton {
-  background: #3c8527;
-  box-shadow: inset 0 -6px 0 #2a641c, inset 0 6px 0 #52a535;
-  color: #ffffff;
-  font-weight: 700;
-  letter-spacing: .03em;
-  text-transform: uppercase;
-
-  cursor: pointer;
-
-  border: 2px solid #262423;
-  display: inline-flex;
-  align-items: center;
-  font-size: 1.125rem;
-  line-height: 1.25rem;
-  min-width: 4.75rem;
-  padding: .9375rem 1.75rem;
-  padding-right: 1rem;
-  text-decoration: none;
-  text-align: center;
-
-  -webkit-user-select: none;
-  user-select: none;
-  text-shadow: 0 2px 0 #505050;
-}
-
-.MCButton:hover {
-  background: linear-gradient(0deg, rgba(0, 0, 0, .1), rgba(0, 0, 0, .1)), #3c8527;
-}
-
-.MCButton:focus:active {
-  box-shadow: 0 0 0 1px #ffffff;
-}
-
-.MCButton:active {
-  background-color: #2a641c;
-}
-
-.MCButton:focus {
-  outline: 2px solid #ffffff;
-  outline-offset: -2px;
-}
-
-.MCButton::after {
-  background-color: #ffffff;
-  clip-path: polygon(0% 0%, 0% 25%, 14.286% 25%, 14.286% 50%, 28.572% 50%, 28.572% 75%, 42.858% 75%, 42.858% 100%, 57.142% 100%, 57.142% 75%, 71.428% 75%, 71.428% 50%, 85.714% 50%, 85.714% 25%, 100% 25%, 100% 0%, 85.714% 0%, 85.714% 25%, 71.428% 25%, 71.428% 50%, 57.142% 50%, 57.142% 75%, 42.858% 75%, 42.858% 50%, 28.572% 50%, 28.572% 25%, 14.286% 25%, 14.286% 0%);
-  content: "";
-  display: inline-block;
-  flex-basis: 14px;
-  flex-grow: 0;
-  flex-shrink: 0;
-  height: 8px;
-  transform: rotate(-90deg);
-  width: 14px;
-  margin-left: 10px;
-}
-</style>
