abc81c38f31182377146f0e7e84a0c8321f8db3a
Misha Gusev <majikx.prod0@gmail.com>
2025-11-05 03:17:23 +0500

    feat: add gear page, update bg in code section

--- src/components/contacts/Contacts.vue
+++ src/components/contacts/Contacts.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
 import { Section, SectionContent } from '@/components/sections'
 import { ContactsData } from '@/data/Contact.data'
-import { Certificate01Icon } from '@untitledui-icons/vue/line'
+import { Certificate01Icon, Settings02Icon } from '@untitledui-icons/vue/line'
 import { ContactsLink } from '.'
 
 const resumeLink = '/cv/'
+const gearLink = '/gear/'
 </script>
 
 <template>
   <Section>
     <SectionContent variant="inline">
-      <ContactsLink
-        :url="resumeLink"
-        name="Резюме"
-        :icon="Certificate01Icon"
-        color="#F6C322"
-      />
+      <ContactsLink :url="resumeLink" name="Резюме" :icon="Certificate01Icon" color="#F6C322" />
+      <ContactsLink :url="gearLink" name="Инструменты" :icon="Settings02Icon" color="#10B981" />
       <ContactsLink
         v-for="contact in ContactsData"
         :key="contact.tag"
